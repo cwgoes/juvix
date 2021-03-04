73c0e75ea092685a92ebb3b96bcb2882ef0826fe
Frank Dedden <frank@heliax.dev>
2021-03-04 14:30:40 +0100

    Implemented transformer for Feedback monad (#692)

--- library/StandardLibrary/src/Juvix/Library/Feedback.hs
+++ library/StandardLibrary/src/Juvix/Library/Feedback.hs
@@ -1,5 +1,6 @@
 module Juvix.Library.Feedback where
 
+import qualified Control.Monad.Trans as Trans
 import Juvix.Library
 
 -- | Keep track of messages during the compilation or REPL.
@@ -27,3 +28,34 @@ instance Monoid msg => Monad (Feedback msg) where
     Succes msgs' x' -> Succes (msgs <> msgs') x'
     Fail msgs' -> Fail (msgs <> msgs')
   Fail msgs >>= _ = Fail msgs
+
+-- | Monad transformer of Feedback.
+data FeedbackT msg m a = FeedbackT {runFeedbackT :: m (Feedback msg a)}
+
+instance Monad m => Functor (FeedbackT msg m) where
+  fmap f mx = FeedbackT $ do
+    x <- runFeedbackT mx
+    return $ fmap f x
+
+instance (Monad m, Monoid msg) => Applicative (FeedbackT msg m) where
+  pure = FeedbackT . pure . pure
+
+  aaf <*> aax = FeedbackT $ do
+    af <- runFeedbackT aaf
+    ax <- runFeedbackT aax
+    return $ af <*> ax
+
+instance (Monad m, Monoid msg) => Monad (FeedbackT msg m) where
+  return = pure
+
+  mmx >>= mmf = FeedbackT $ do
+    mx <- runFeedbackT mmx
+    case mx of
+      Succes msgs x -> runFeedbackT $ mmf x
+      Fail msgs -> return $ Fail msgs
+
+instance Monoid msg => Trans.MonadTrans (FeedbackT msg) where
+  lift mx = FeedbackT $ mx >>= return . return
+
+instance (MonadIO m, Monoid msg) => MonadIO (FeedbackT msg m) where
+  liftIO = lift . liftIO
